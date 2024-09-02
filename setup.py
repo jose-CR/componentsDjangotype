8c8f9a165e9bd8c139bee036a89d11f303935219
crowend <hjosealejandro21@gmail.com>
2024-09-01 22:54:10 -0600

    fixed of Manifies.in

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='cmponentsDjangoType',
+    name='componentsDjangoType',
     version='0.0.1',
     packages=find_packages(),
     include_package_data=True,
     license='MIT',
-    description='A Django app for ...',
+    description='A Django app for creating HTML components',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/jose-CR/componentsDjangotype',
+    url='https://github.com/jose-CR/componentsDjangoType',
     author='Alejandro',
-    author_email='',
+    author_email='your-email@example.com',
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
