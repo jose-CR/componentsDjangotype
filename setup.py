5dd83e7d961173093cfde471a38750126135b459
crowends <hjosealejandro21@gmail.com>
2024-11-18 13:17:07 -0600

    creating the files for the views

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='componentsDjangoType',
-    version='1.0.0',
+    version='1.0.5',
     packages=find_packages(),
     include_package_data=True,
     license='MIT',
