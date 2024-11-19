542b3117fc81e446969b775856191a6ee53b3163
crowends <hjosealejandro21@gmail.com>
2024-11-18 19:05:56 -0600

    fixed it the spaces of files

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='componentsDjangoType',
-    version='1.0.8',
+    version='1.0.9',
     packages=find_packages(),
     include_package_data=True,
     license='MIT',
